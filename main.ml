42bd381774ff81372161abba75af4e69ce0c2c99
0918nobita <nobita.0918@gmail.com>
2018-12-27 01:57:54 +0900

    :sparkles: float に関するサンプルコードを追加した

--- main.ml
+++ main.ml
@@ -1,2 +1,17 @@
+(*
+  # 1e350;;
+  - : float = infinity
+  # -3e400;;
+  - : float = neg_infinity
+  2.0 /. 0.;;
+  - : float = infinity
+  0. /. 0.;;
+  - : float = nan
+  # sqrt (-1.);;
+  - : float = nan
+  # infinity +. neg_infinity
+  - : float = nan
+*)
+
 let () =
   Printf.printf "Hello, OCaml!\n"
