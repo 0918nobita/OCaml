9818a7e83b839cf7aca8b01bfc3760a5fe436288
0918nobita <nobita.0918@gmail.com>
2018-12-28 18:06:13 +0900

    :sparkles: 練習問題3.1の解答を追加した

--- src/main.ml
+++ src/main.ml
@@ -17,5 +17,24 @@
   - : char = 'o'
 *)
 
+(* 練習問題 3.1 *)
+
+(* (1) US ドル (実数) を受け取って円 (整数) に換算する関数 (四捨五入) *)
+let conv dollar = int_of_float (Float.round (dollar *. 114.32));;
+
+(* (2) 円 (整数) を受け取って、US ドル (セント以下を小数にした実数) に換算する関数 *)
+(* 1 セント以下は四捨五入 *)
+let conv2 yen = Float.round ((float_of_int yen) /. 114.32)
+
+(* (3) US ドル (実数) を受け取って "[ドル] dollars are [円] yen." という文字列を返す関数 *)
+let conv3 dollar =
+  (string_of_float dollar) ^ " dollars are " ^ (string_of_int (conv dollar)) ^ " yen."
+
+(* (4) 文字を受け取って、アルファベットの小文字なら大文字に、その他の文字はそのまま帰す関数 *)
+let capitalize c =
+  if 'a' <= c && c <= 'z'
+    then (char_of_int ((int_of_char c) - 32))
+    else c
+
 let () =
   Printf.printf "Hello, OCaml!\n"
