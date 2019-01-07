565fe45c25cee1c7ce2505db86406e91d8fc2507
0918nobita <nobita.0918@gmail.com>
2019-01-07 21:28:52 +0900

    :sparkles: string_of_nat 関数を追加した

--- CoPL/dtgen.ml
+++ CoPL/dtgen.ml
@@ -3,3 +3,7 @@ type nat = Z | S of nat
 type judgement = Plus of nat * nat * nat
 
 type derivation_tree = { rule: string; conclusion : judgement; premise : derivation_tree list }
+
+let rec string_of_nat = function
+    Z -> "Z"
+  | S (n') -> "S(" ^ (string_of_nat n') ^ ")"
