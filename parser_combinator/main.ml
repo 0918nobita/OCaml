315404a843ee0430777dfb0acadbdc4145226d8d
0918nobita <nobita.0918@gmail.com>
2019-01-30 21:36:58 +0900

    :sparkles: 0 回以上の繰り返しのパーサを生成する many 関数を追加した

--- parser_combinator/main.ml
+++ parser_combinator/main.ml
@@ -1,6 +1,6 @@
-type ast = Str of string | Seq of ast list
+type ast = Str of string
 
-type result = Success of (ast * string * int) | Failure
+type result = Success of (ast list * string * int) | Failure
 
 type parser = string -> int -> result
 
@@ -14,6 +14,13 @@ let token str target position =
   let str_len = String.length str in
     match substr target position str_len with
         Some cut -> if cut = str
-          then Success (Str str, target, position + str_len)
+          then Success ([Str str], target, position + str_len)
           else Failure
       | None -> Failure
+
+let many parse target position =
+  let rec many_inner parse target position ast_list =
+    match parse target position with
+        Success (ast, str, p) -> many_inner parse str p @@ ast_list @ ast
+      | Failure -> Success (ast_list, target, position) in
+  many_inner parse target position []
