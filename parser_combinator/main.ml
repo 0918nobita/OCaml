30d58a9933c640d0ca10cc14fcd00e04684b5ca4
0918nobita <nobita.0918@gmail.com>
2019-01-30 18:02:22 +0900

    :sparkles: 文字列のパーサを生成する token 関数を追加した

--- parser_combinator/main.ml
+++ parser_combinator/main.ml
@@ -6,3 +6,10 @@ let parseHoge target position =
   if length target >= 4 && sub target 0 4 = "hoge"
     then Success ("hoge", position + 4)
     else Failure
+
+let token str =
+  let len = length str in
+    fun target position ->
+      if length target >= length str + position && sub target position len = str
+        then Success (str, position + len)
+        else Failure
