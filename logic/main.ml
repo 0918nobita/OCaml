ccc041621126ff607f25a5ed3bcabc8236fa7093
0918nobita <nobita.0918@gmail.com>
2019-04-24 00:57:37 +0900

    :sparkles: logical_expr 型を追加した

--- logic/main.ml
+++ logic/main.ml
@@ -1,1 +1,8 @@
-let () = print_endline "hello, world"
+type logical_expr =
+    True
+  | False
+  | And of logical_expr * logical_expr
+  | Or of logical_expr * logical_expr
+  | Not of logical_expr
+  | Imp of logical_expr * logical_expr
+  | Var of string
