b8a004ae6b05d824e39cd7e973ab73ece1c1f036
0918nobita <nobita.0918@gmail.com>
2019-04-24 00:58:07 +0900

    :sparkles: string_of_expr 関数を実装した

--- logic/main.ml
+++ logic/main.ml
@@ -8,3 +8,12 @@ type logical_expr =
   | Var of string
 
 let enclose str = "(" ^ str ^ ")"
+
+let rec string_of_expr = function
+    True -> "t"
+  | False -> "f"
+  | And (lhs, rhs) -> enclose @@ string_of_expr lhs ^ " ∧ " ^ string_of_expr rhs
+  | Or (lhs, rhs) -> enclose @@ string_of_expr lhs ^ " ∨ " ^ string_of_expr rhs
+  | Not (expr) -> "¬" ^ enclose @@ string_of_expr expr
+  | Imp (lhs, rhs) -> enclose @@ string_of_expr lhs ^ " ⇒ " ^ string_of_expr rhs
+  | Var (name) -> name
