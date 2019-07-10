782ac0b7c164b6b2f58fd89087058f22ceb3015b
0918nobita <nobita.0918@gmail.com>
2019-07-10 16:41:50 +0900

    :sparkles: 参照型データに依存するクラスのサンプルを追加した

--- oop/main.ml
+++ oop/main.ml
@@ -11,3 +11,19 @@ let () =
   p#move 3;
   p#move 4;
   print_endline @@ string_of_int @@ p#get_x (* => 7 *)
+
+let c = ref 0
+
+class counter_class =
+  object
+    val mutable count = incr c; !c
+    method get_count = count
+    method increase diff = count <- count + diff
+  end
+
+let counter1 = new counter_class
+let counter2 = new counter_class
+
+let () =
+  print_endline @@ string_of_int @@ counter1#get_count; (* => 1 *)
+  print_endline @@ string_of_int @@ counter2#get_count (* => 2 *)
