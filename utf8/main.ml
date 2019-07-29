25a29a5000bd4c6383f25e0d80c8f998fb0b1cdd
0918nobita <nobita.0918@gmail.com>
2019-07-29 21:53:04 +0900

    :sparkles: Base モジュールを用いて UChar / Text モジュールを実装し直した

--- utf8/main.ml
+++ utf8/main.ml
@@ -1,8 +1,12 @@
+open Base
+
 module UChar : sig
 
   type t
 
   val of_char : char -> t
+
+  val of_bytes : int list -> t
 
   val string_of : t -> string
 
@@ -10,104 +14,58 @@ end = struct
 
   type t = int array
 
-  let of_char c = [| Char.code c |]
+  let of_char c = [| Char.to_int c |]
+
+  let of_bytes = Array.of_list
 
   let string_of uchar =
-    String.init
-      (Array.length uchar)
-      (fun n -> char_of_int @@ Array.get uchar n)
-
+    String.init (Array.length uchar) (fun n -> Char.unsafe_of_int @@ uchar.(n))
 end
 
-(*
 module Text : sig
 
   type t
 
-  val of_string : string -> t
-
   val length : t -> int
 
-  val get : t -> int -> t option
+  val get : t -> int -> UChar.t
 
-  val get_exn : t -> int -> t
+  val of_string : string -> t
 
 end = struct
 
-  type t = int array
+  type t = UChar.t array
+
+  let length = Array.length
+
+  let get = Array.get
 
   let of_string str =
-    Array.init
-      (String.length str)
-      (fun n -> int_of_char @@ String.get str n)
-
-  let length text =
-    match Array.length text with
-      | 0 -> 0
+    match String.length str with
+      | 0 -> [| |]
       | bytes ->
-        let count = ref 0 in
-        let pos = ref 0 in
-        while !pos < bytes do
-          count := !count + 1;
-          let ascii = text.(!pos) in
-          if ascii land 0x80 = 0
-            then pos := !pos + 1
-            else
-              let tmp = ref (ascii land 0xFC) in
-              while !tmp land 0x80 != 0 do
-                pos := !pos + 1;
-                tmp := !tmp lsl 1
-              done
-        done;
-        !count
-
-  exception Out_of_loop of int array
-
-  let unwrap = function
-    | Some v -> v
-    | _ -> raise @@ Invalid_argument "Unwrap failure"
-
-  let get text i =
-    match Array.length text with
-      | 0 -> None
-      | bytes ->
-        let index = ref (-1) in
-        let pos = ref 0 in
-        try
+          let uchars = ref [] in
+          let pos = ref 0 in
           while !pos < bytes do
-            index := !index + 1;
-            let ascii = text.(!pos) in
+            let ascii = Char.to_int @@ String.get str !pos in
             if ascii land 0x80 = 0
               then
                 begin
                   pos := !pos + 1;
-                  if i = !index then raise @@ Out_of_loop [| ascii |]
+                  uchars := UChar.of_bytes [ascii] :: !uchars;
                 end
               else
                 begin
+                  let bytes = ref [] in
                   let tmp = ref (ascii land 0xFC) in
-                  let array = ref [| |] in
-                  while !tmp land 0x80 != 0 do
+                  while not (!tmp land 0x80 = 0) do
+                    bytes := (Char.to_int @@ String.get str !pos) :: !bytes;
                     pos := !pos + 1;
-                    array := Array.append !array [| !tmp |];
-                    tmp := !tmp lsl 1
+                    tmp := !tmp lsl 1;
                   done;
-                  if i = !index then (Array.iter (fun i -> print_endline @@ string_of_int i) !array; raise @@ Out_of_loop !array)
+                  uchars := UChar.of_bytes (List.rev !bytes) :: !uchars
                 end
           done;
-          None
-        with
-          | Out_of_loop text -> Some text
-
-  let get_exn text i = unwrap @@ get text i
-
-  let sub text start len =
-    let text_len = length text in
-    if text_len >= start + len
-      then Some ()
-      else None
+          Array.of_list @@ List.rev !uchars
 
 end
-
-let () = print_endline @@ string_of_int @@ Text.length @@ Text.of_string "あいうえお"  (* => 5 *)
-*)
